0dd478dcdc788c83f7ca82a03e8c29c62c4f88aa
Lidia Gonzalez <lidia_1508@hotmail.com>
2025-03-19 17:59:54 +0000

    Create component for hamburguer menu logic

---
+++ app/components/HamburguerMenu.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="relative flex md:hidden">
+    <button @click="handleMenuClick">
+      <IconMenuRounded class="size-10" />
+    </button>
+
+    <transition name="fade">
+      <div
+        v-if="isOpen"
+        class="absolute right-0 mt-2 w-48 bg-slate-800 text-slate-100 p-4 flex flex-col gap-2 rounded-lg shadow-lg shadow-gray-950"
+      >
+        <a
+          v-for="option in menuOptions"
+          :href="option.href"
+          class="p-2 rounded-lg transition-colors active:bg-slate-900"
+          @click="handleOptionClick"
+        >
+          {{ option.name }}
+        </a>
+      </div>
+    </transition>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { menuOptions } from "../data/MenuOptions";
+
+defineProps({
+  isOpen: {
+    type: Boolean,
+    required: true,
+  },
+  handleMenuClick: {
+    type: Function as PropType<() => void>,
+    required: true,
+  },
+  handleOptionClick: {
+    type: Function as PropType<() => void>,
+    required: true,
+  },
+});
+</script>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.2s ease-out;
+}
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
--- app/components/Navbar.vue
+++ app/components/Navbar.vue
@@ -24,27 +24,12 @@
       </li>
     </ul>
 
-    <div class="relative md:hidden" ref="menuRef">
-      <button @click="isOpen = !isOpen">
-        <IconMenuRounded class="size-10" />
-      </button>
-
-      <transition name="fade">
-        <div
-          v-if="isOpen"
-          class="absolute right-0 mt-2 w-48 bg-slate-800 text-slate-100 p-4 flex flex-col gap-2 rounded-lg shadow-lg shadow-gray-950"
-        >
-          <a
-            v-for="option in menuOptions"
-            :href="option.href"
-            class="p-2 rounded-lg transition-colors active:bg-slate-900"
-            @click="isOpen = false"
-          >
-            {{ option.name }}
-          </a>
-        </div>
-      </transition>
-    </div>
+    <HamburguerMenu
+      ref="menuRef"
+      :is-open="isOpen"
+      :handle-menu-click="handleMenuClick"
+      :handle-option-click="handleOptionClick"
+    />
   </nav>
 </template>
 
@@ -59,18 +44,19 @@ const middleIndex = computed(() => Math.ceil(menuOptions.length / 2));
 const leftMenu = computed(() => menuOptions.slice(0, middleIndex.value));
 const rightMenu = computed(() => menuOptions.slice(middleIndex.value));
 
+const handleMenuClick = () => {
+  isOpen.value = !isOpen.value;
+};
+
+const handleOptionClick = () => {
+  isOpen.value = false;
+};
+
 onClickOutside(menuRef, () => {
   isOpen.value = false;
 });
 </script>
 
 <style scoped>
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity 0.2s ease-out;
-}
-.fade-enter-from,
-.fade-leave-to {
-  opacity: 0;
-}
+
 </style>
